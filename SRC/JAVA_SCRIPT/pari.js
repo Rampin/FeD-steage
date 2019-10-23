65353b36b4720fb8a4d7a7bb5080f0bc460c4cf8
Rampin <ramp1996@gmail.com>
2019-10-23 17:28:54 +0200

    23/10/2019

--- SRC/JAVA_SCRIPT/pari.js
+++ SRC/JAVA_SCRIPT/pari.js
@@ -1,19 +1,37 @@
 function isEven(a) {
-	if (typeof a !== 'number' || a - Math.floor(a) !== 0) {
-		return undefined;
-	} else if (a === 0) {
-		return true;
-	} else {
-		if (a % 2 !== 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+  if (typeof a !== "number" || a - Math.floor(a) !== 0 || a < 0) {
+    return undefined;
+  } else if (a === 0) {
+    return true;
+  } else {
+    if (a % 2 !== 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
+/*
+console.log(isEven(2.5) === undefined);
+console.log(isEven('ciao') === undefined);
+console.log(isEven(0) === true);
+console.log(isEven(7) === false);
+console.log(isEven(12) === true);
+console.log(isEven(-4) === undefined);
+*/
 
-console.log(isEven(2.5)===undefined);
-console.log(isEven('ciao')===undefined);
-console.log(isEven(0)===true);
-console.log(isEven(7)===false);
-console.log(isEven(12)===true);
+
+{
+  let v = 0;
+  function Speed(d, t) {
+    if (typeof d !== "number" || typeof t !== "number" || d < 0 || t <= 0) {
+      return undefined;
+    } else {
+      v = d / t;
+    }
+  }
+  let d=6;
+  let t=3;
+  Speed(d, t);
+  console.log(v);
+}
