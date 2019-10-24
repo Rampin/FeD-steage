db02ee074cb2076a72f7868c62d83d6d055a5f81
Rampin <ramp1996@gmail.com>
2019-10-24 10:31:38 +0200

    24/10/2019

--- SRC/JAVA_SCRIPT/digitSum.js
+++ SRC/JAVA_SCRIPT/digitSum.js
@@ -1,20 +1,19 @@
 function digitSum(n) {
-    let sum = 0;
-    for (n; n >= 1; n = Math.floor(n / 10)) {
-      sum = sum + (n % 10);
-    }
-    return sum;
+  let sum = 0;
+  for (n; n >= 1; n = Math.floor(n / 10)) {
+    sum = sum + (n % 10);
   }
-  
-  function digitSum2(n) {
-    n = n.toString();
-    let sum2 = 0;
-    for (let i = 0; i < n.length; ++i) {
-      sum2 = sum2 + Number(n[i]);
-    }
-    return sum2;
+  return sum;
+}
+
+function digitSum2(n) {
+  n = n.toString();
+  let sum2 = 0;
+  for (let i = 0; i < n.length; ++i) {
+    sum2 = sum2 + Number(n[i]);
   }
-  
-  //console.log(digitSum(151));
-  console.log(digitSum2(351));
-  
+  return sum2;
+}
+
+//console.log(digitSum(151));
+console.log(digitSum2(351));
