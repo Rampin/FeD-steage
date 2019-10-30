69ebcf644bc2156df05bd451a30b8998d9767a5b
Rampin <ramp1996@gmail.com>
2019-10-30 11:48:52 +0100

    30/10/2019

---
+++ SRC/JAVA_SCRIPT/fibonacci.js
@@ -0,0 +1,30 @@
+function fibonacci(x) {
+    let fibo = [];
+    for (let i = 0; i <= x; i++) {
+        if (i === 0 || i === 1) { fibo[i] = 1; }
+        else { fibo[i] = (fibo[i - 1]) + (fibo[i - 2]); }
+    }
+    return fibo[x];
+}
+
+
+
+
+
+function fibonacci2(x) {
+    let a=[1,1];
+    
+    if (x === 0 || x === 1) {
+        return a[x];
+    }
+    else {
+        if(a[x]==undefined){
+            a[x]=fibonacci2(x - 2) + fibonacci2(x - 1);
+        return a[x];}
+        else {return a[x];}
+    }
+        
+
+}
+
+console.log(fibonacci2(10));
